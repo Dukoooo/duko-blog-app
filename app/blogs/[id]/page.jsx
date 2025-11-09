154fe1dd73559a420339be4ad826026619cdcd3e
Juraj Golian <118574181+Dukoooo@users.noreply.github.com>
2025-11-09 22:06:05 +0100

    feat: page route css

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,8 +1,10 @@
 "use client";
-import { blog_data } from "@/assets/assets";
-import { useEffect, useState } from "react";
+import { assets, blog_data } from "@/assets/assets";
+import Image from "next/image";
+import { useEffect, useState, use } from "react";
 
-function page({ params }) {
+function page(props) {
+  const params = use(props.params);
   const [data, setData] = useState(null);
 
   function fetchBlogData() {
@@ -19,10 +21,41 @@ function page({ params }) {
     fetchBlogData();
   }, []);
 
-  return (
-    <div className="">
-      <h1>kokot</h1>
-    </div>
+  return data ? (
+    <>
+      <div className="bg-gray-300 py-5 px-5 md:px-12 lg:px-28 ">
+        <div className="flex justify-between items-center">
+          <Image src={assets.logo} width={180} alt="logo" className="w-[130]" />
+          <button className="flex items-center gap-2 font-medium py-1 px-3  sm:px-3 border border-black shadow-[-2px_2px_0_#000]">
+            Spat
+          </button>
+        </div>
+        <div className=" text-center my-24">
+          <h1 className="font-black text-4xl text-center mt-10">
+            {data.title}
+          </h1>
+          <h4 className="text-center mt-5 tracking-wider font-[Roboto_Mono] font-black">
+            {data.author}
+          </h4>
+          <Image
+            src={data.author_img}
+            width={100}
+            height={100}
+            className="mx-auto my-6 rounded-full  "
+          />
+        </div>
+      </div>
+      <div className="mt-[-100px] mx-5 max-w-[800px] md:mx-auto mb-10">
+        <Image
+          src={data.image}
+          width={1280}
+          height={720}
+          className="mx-auto mt-10 "
+        />
+      </div>
+    </>
+  ) : (
+    <></>
   );
 }
 
